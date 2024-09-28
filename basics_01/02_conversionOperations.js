f364bfe5c3b30145f61f1681534ba699bf747ae2
ISHRAT <45869750+Ishrat06Qureshi@users.noreply.github.com>
2024-09-28 15:26:19 +0000

    type conversions covered

---
+++ basics_01/02_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = undefined;
+// console.log(typeof score);
+
+let valueInString = String( score);
+// console.log(valueInString);
+
+// console.log(typeof valueInString);
+
+// String === Number
+
+let userName = ''
+let valueInNumber  = Number(userName);
+
+// Boolean == Number
+
+let isLoggedIn = true
+let booleanNumberInValue = Number(isLoggedIn)
+
+// Number == Boolean
+let status = ''
+
+let numberValueInBoolean = Boolean(status)
+console.log(numberValueInBoolean);
+console.log(typeof numberValueInBoolean);
+
+
