d53cc352c052b5aaa6d836a28d05a51275693e6d
ISHRAT <45869750+Ishrat06Qureshi@users.noreply.github.com>
2024-09-28 14:44:58 +0000

    dataype covered

---
+++ basics_01/01_dataTypes.js
@@ -0,0 +1,14 @@
+let userName = "ishrat"
+let age = 28;
+let isLoggedIn = false;
+
+/* data types are listed below
+Number
+BigInt
+String
+Array 
+Object
+Null --> stand alone value -- empty value
+
+*/
+//typeof operator can be use for finding the datatype
