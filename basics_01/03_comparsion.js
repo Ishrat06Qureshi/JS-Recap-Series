6d4532aa7890c364b07eb5acf7c9da2a8c17439c
ISHRAT <45869750+Ishrat06Qureshi@users.noreply.github.com>
2024-09-29 17:01:07 +0000

    comparsion And DataTypeSummary

---
+++ basics_01/03_comparsion.js
@@ -0,0 +1,16 @@
+// comparison with string and number
+
+//console.log('2' > 3);
+//console.log(3 > '2' );
+
+//Comparsion of Number with boolean
+//console.log(true > 2);
+//console.log(false == 0);
+
+//Comparsion of string with boolean
+console.log('1' >=true);
+
+
+
+
+
---
+++ basics_01/dataTypeSummary.js
@@ -0,0 +1,20 @@
+// Primitive data Type 
+// store by value == actual value is store in the variable
+//Number , String ,Boolean , Null ,undefined , 
+//store by reference == the pointer to the memory is store in the variable ---> consider below example
+
+
+/*ObjOne --> objTwo -- both points to simillar object stored in the memory --> Obj ==> 
+  
+    let objOne  = {
+        firstName: "ishu"
+            
+    }
+    let objTwo = objOne;
+    objTwo.firstName = 'nambeel'
+    console.log(objOne);
+    */
+   let userName = 'Nabeel'
+   let age  = 28
+
+
